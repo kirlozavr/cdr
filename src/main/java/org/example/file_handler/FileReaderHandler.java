5726054ebd8e8fb3496fc9b8b991b1bd2b36dead
kirlozavr <kirill.protchenko@mail.ru>
2023-03-25 14:20:55 +1000

    Класс для чтения данных из файла и записи их в БД.

---
+++ src/main/java/org/example/file_handler/FileReaderHandler.java
@@ -0,0 +1,42 @@
+package org.example.file_handler;
+
+import org.example.database.DataBaseHelper;
+import org.example.entity.SubscriberEntity;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class FileReaderHandler {
+
+    private FileReader fileReader;
+    private BufferedReader bufferedReader;
+    private DataBaseHelper dataBaseHelper;
+
+    public FileReaderHandler(String fileName) throws FileNotFoundException {
+        fileReader = new FileReader(fileName);
+        bufferedReader = new BufferedReader(fileReader);
+        dataBaseHelper = new DataBaseHelper();
+    }
+
+    public void readAndInsertToDataBase() throws SQLException, ClassNotFoundException, IOException {
+        dataBaseHelper.connection();
+        dataBaseHelper.crateTable(DataBaseHelper.CREATE_TABLE_SUBSCRIBER);
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null ){
+            String[] lineArr = line.split(", ");
+            SubscriberEntity subscriberEntity = new SubscriberEntity(
+                    lineArr[1],
+                    lineArr[0],
+                    lineArr[2],
+                    lineArr[3],
+                    lineArr[4]
+            );
+            dataBaseHelper.insert(subscriberEntity);
+        }
+        dataBaseHelper.close();
+    }
+}
