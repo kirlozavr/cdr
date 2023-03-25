752779d426961910836451e0bbc2e686ac71b18e
kirlozavr <kirill.protchenko@mail.ru>
2023-03-25 19:25:04 +1000

    Запуск генератора отчетов, перед этим данные с файла были записаны в БД

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.sql.SQLException;
 
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
+        ReportGenerator reportGenerator = new ReportGenerator();
+        reportGenerator.getDistinctionEntity();
+        reportGenerator.writeReportFiles();
+        reportGenerator.closeDB();
     }
 }
